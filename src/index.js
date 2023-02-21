e1ded360126d470af92570470d7046d71d790d08
MD MOQADDAS <sgn9556@gmail.com>
2023-02-22 00:35:23 +0530

    new app

--- src/index.js
+++ src/index.js
@@ -1,19 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-<<<<<<< HEAD
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-=======
-
-
- 
->>>>>>> 24eda4476d2aba4bf36cca4395a67225d91475e3
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-<<<<<<< HEAD
     <App />
   </React.StrictMode>
 );
@@ -22,9 +15,3 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-=======
-  <h1>Hi Welcome to the React Application</h1>
-  </React.StrictMode>
-);
-
->>>>>>> 24eda4476d2aba4bf36cca4395a67225d91475e3
